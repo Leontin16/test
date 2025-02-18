574987e25e400456d1539cd5ba9d0ac7171b2994
Leontin16 <74112814+Leontin16@users.noreply.github.com>
2025-02-18 22:10:03 +0300

    Add files via upload

---
+++ тестовое1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\тестовое1\func.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::IsTrue(add(1, 2) == 3);
+		}
+		TEST_METHOD(TestMethod2) 
+		{
+			Assert::IsTrue(add(-1, 1) == 0);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::IsTrue(add(-1, -1) == -2);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::IsTrue(add(0, 0) == 0);
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			Assert::IsTrue(add(5, 9) == 14);
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			Assert::IsTrue(add(5, 10) == 15);
+		}
+	};
+}
---
+++ тестовое1/тестовое1/Source.cpp
@@ -0,0 +1,9 @@
+#include "func.h"
+int main()
+{
+	int a, b, res;
+	cin >> a >> b;
+	res = add(a, b);
+	cout << res << endl;
+	return 0;
+}
